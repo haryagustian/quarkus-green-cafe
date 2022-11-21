bd95772216d4899d7178ad16819871eeb86bf3f9
Hary Agustian <tech.hary.agustian@gmail.com>
2022-11-21 18:31:52 +0700

    testing auth controller

--- src/test/java/org/green/cafe/controllers/AuthControllerTest.java
+++ src/test/java/org/green/cafe/controllers/AuthControllerTest.java
@@ -1,15 +1,19 @@
 package org.green.cafe.controllers;
 
+import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import io.quarkus.test.security.TestSecurity;
+import org.junit.jupiter.api.*;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,20 +24,54 @@ class AuthControllerTest {
 
   @Test
   @Order(1)
+  @TestSecurity(user = "testUser", roles = "user")
   void loginSuccess() {
-    when()
+    JsonObject jsonObject = Json
+        .createObjectBuilder()
+        .add("loginName", "haryaugust101")
+        .add("password", BcryptUtil.bcryptHash("Areuaoneorazero10"))
+        .build();
+
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(jsonObject.toString())
+        .when()
+        .post()
+        .then()
+        .assertThat()
+        .statusCode(Response.Status.BAD_REQUEST.getStatusCode())
+        .body("size()", equalTo(1))
+        .body("[0].id", equalTo(1))
+        .body("[0].loginName", equalTo(1))
+        .body("[0].password", equalTo(BcryptUtil.bcryptHash("Areuaoneorazero10")));
+  }
+
+  @Test
+  @Order(2)
+  void loginFailed() {
+    JsonObject jsonObject = Json
+        .createObjectBuilder()
+        .add("loginName", "haryagustian69")
+        .add("password", BcryptUtil.bcryptHash("rahasisa69"))
+        .build();
+
+    given()
+        .contentType(MediaType.APPLICATION_JSON)
+        .body(jsonObject.toString())
+        .when()
         .post()
         .then()
         .assertThat()
         .statusCode(Response.Status.OK.getStatusCode())
-        .body("size()", is(0));
-  }
-
-  @Test
-  void loginFailed() {
+        .body("size()", equalTo(1))
+        .body("[0].id", equalTo(1))
+        .body("[0].loginName", equalTo(1))
+        .body("[0].password", equalTo(BcryptUtil.bcryptHash("rahasia69")));
   }
 
   @Test
   void builder() {
   }
+
+
 }
